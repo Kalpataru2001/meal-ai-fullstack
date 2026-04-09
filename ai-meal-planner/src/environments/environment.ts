a581b8fa8369c3d8bef527ad563b422b43757796
Kalpatarusahoo <Kalpataru.sahoo@kalpitatechnologies.com>
2026-04-09 10:07:19 +0530

    Fix hidden localhost in production build

--- ai-meal-planner/src/environments/environment.ts
+++ ai-meal-planner/src/environments/environment.ts
@@ -3,5 +3,5 @@ export const environment = {
   production: false,
   supabaseUrl: 'https://ieickkundehlfpsbhrev.supabase.co',
   supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImllaWNra3VuZGVobGZwc2JocmV2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ5MzgxMTQsImV4cCI6MjA5MDUxNDExNH0.3lGp6sVthR6tYrIUifsfRnwRKjt2pEzaIlQ353jkZ-Y',
-  apiUrl: 'http://localhost:5125/api'
+  apiUrl: 'https://mealaiapi.onrender.com'
 };
